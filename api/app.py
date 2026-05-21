ddf283210fae84078b8b30ffbd1fa1f30f05d0c6
KevinStiven04 <kevinmontoya0614@gmail.com>
2026-05-21 15:58:13 -0500

    Feat: Cambio en APP para el despliegue

--- api/app.py
+++ api/app.py
@@ -25,10 +25,7 @@ app = FastAPI(title="API", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:4200",
-        "http://127.0.0.1:4200"
-    ],
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
