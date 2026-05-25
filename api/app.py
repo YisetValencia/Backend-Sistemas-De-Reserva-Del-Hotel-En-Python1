a4028fc994b3ff8265f46ee585b767541b8d9413
KevinStiven04 <kevinmontoya0614@gmail.com>
2026-05-25 15:36:49 -0500

    Feat: Cambio par el frontend

--- api/app.py
+++ api/app.py
@@ -25,7 +25,9 @@ app = FastAPI(title="API", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=[
+        "https://frontend-y7g3.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
